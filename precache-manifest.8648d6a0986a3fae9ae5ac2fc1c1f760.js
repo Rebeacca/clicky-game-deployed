cebc7141ee6c9acbb3fed0b5575abfe23f445ee7
Rebeacca Modayil <rebeacca.modayil@temple.edu>
2019-10-09 20:32:44 -0400

    Updates

---
+++ precache-manifest.8648d6a0986a3fae9ae5ac2fc1c1f760.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b8db51de27edfd39e7b952fa0af6eb",
+    "url": "/clicky-game-deployed/index.html"
+  },
+  {
+    "revision": "5db27496d25b1c25ec78",
+    "url": "/clicky-game-deployed/static/css/main.f01baa36.chunk.css"
+  },
+  {
+    "revision": "726e9aeccac1b8bf5551",
+    "url": "/clicky-game-deployed/static/js/2.99ed8246.chunk.js"
+  },
+  {
+    "revision": "5db27496d25b1c25ec78",
+    "url": "/clicky-game-deployed/static/js/main.1c0c8a54.chunk.js"
+  },
+  {
+    "revision": "57b504182c487233c120",
+    "url": "/clicky-game-deployed/static/js/runtime-main.b9a0017a.js"
+  }
+]);
